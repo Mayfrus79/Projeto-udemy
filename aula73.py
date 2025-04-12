f621e780b16dcd593a941c5c022da64a262ca559
maykon jonattan <mayfrusmj@gmail.com>
2025-04-12 15:12:23 -0300

    modification in file aula73.py

---
+++ aula73.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument(
+    '-b', '--basic',
+    help='Displays "Hello world" on the screen',
+    # type=str, # Argument type
+    metavar='STRING',
+    # default='Hello world', # Default value
+    required=False,
+    action='append',  # Accepts the argument more than once
+    # nargs='+', # Accepts more than one value
+)
+parser.add_argument(
+    '-v', '--verbose',
+    help='Displays logs',
+    action='store_true'
+)
+args = parser.parse_args()
+
+if args.basic is None:
+    print('You did not provide the value for b.')
+    print(args.basic)
+else:
+    print('The value of basic:', args.basic)
+
+print(args.verbose)
