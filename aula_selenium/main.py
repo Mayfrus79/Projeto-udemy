243e9e6f6f02c78d0ffce4abcec2098c3baa8df6
maykon jonattan <mayfrusmj@gmail.com>
2025-04-23 17:01:37 -0300

    Modified the file, i'm still don't know what i'm doing

--- aula_selenium/main.py
+++ aula_selenium/main.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
+# Abre o navegador
 browser = webdriver.Edge()
+browser.get("https://best.aliexpress.com/?dp=BBS11&af=2015&aff_fcid=85a949a8588f4238918bd940e8334140-1745437789585-00586-_Dlppdsf&tt=CPS_NORMAL&aff_fsk=_Dlppdsf&aff_platform=portals-tool&sk=_Dlppdsf&aff_trace_key=85a949a8588f4238918bd940e8334140-1745437789585-00586-_Dlppdsf&terminal_id=a502c334fcbc4144ab650657be6a8208")
+browser.maximize_window()
 
-browser.get('https://google.com.br/')
+# Localiza a barra de pesquisa e digita algo
+search_box = browser.find_element(By.ID, "search-words")
+search_box.click()
+search_box.send_keys("The beginning after the end")  # Altere para o que você quiser buscar
+search_box.send_keys(Keys.RETURN)     # Pressiona Enter para buscar
 
-time.sleep(10)
+
+
+
+# Aguarda resultados
+time.sleep(500)
