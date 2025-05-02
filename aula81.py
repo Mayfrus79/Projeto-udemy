f154b8a3b04dafe8e79047ac1d94541005c7fb99
Maykon Jonattan <mayfrusmj@gmail.com>
2025-05-01 23:37:25 -0300

    just a little alteration in the name of the folder's path

--- aula81.py
+++ aula81.py
@@ -9,14 +9,14 @@ parking.append({"Plate": "BBB222", "Model": "Palio", "Color": "Red"})
 parking.append({"Plate": "CCC333", "Model": "Gol", "Color": "Black"})
 parking.appendleft({"Plate": "DDD444", "Model": "Fox", "Color": "White"})
 
-FOLDER_PATH = Path.home() / "Desktop" / "Json_folder"
-FOLDER_PATH.mkdir(exist_ok=True)
-JSON_FILE = FOLDER_PATH / "Json_file.json"
+folder_path = Path.home() / "Desktop" / "Json_folder"
+folder_path.mkdir(exist_ok=True)
+JSON_FILE = folder_path / "Json_file.json"
 
 if JSON_FILE.exists():
-    print(f"The file '{JSON_FILE.name}' already exist in the folder '{FOLDER_PATH.name}'.")
+    print(f"The file '{JSON_FILE.name}' already exist in the folder '{folder_path.name}'.")
 
 else:
     with open(JSON_FILE, 'w', encoding='utf-8') as file:
         json.dump(list(parking), file, indent=3, ensure_ascii=False)
-        print(f"File saved as: '{JSON_FILE.name}' in the folder: '{FOLDER_PATH.name}'")    
+        print(f"File saved as: '{JSON_FILE.name}' in the folder: '{folder_path.name}'")    
